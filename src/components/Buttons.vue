382d1f1f5d8ea995fb18bd984e92b0399801a282
AdamGibbs <adam-gibbs@outlook.com>
2021-12-20 16:25:48 +0000

    initial

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,33 +1,17 @@
-<template>    
-    <div class="m-4 flex">
-        <button
-        @click="subscribeToggle"
-        class="
-            bg-midGreen
-            hover:bg-startGrad
-            text-white text-center
-            font-black
-            px-4 py-2 mx-5
-            rounded-full
-            float-left
-        "
-        >
-        Sign up for updates
-        </button>
-        <button
-        class="
-            bg-midGreen
-            hover:bg-startGrad
-            text-white text-center
-            font-black
-            px-4 py-2 mx-5
-            rounded-full
-            float-right
-            "
-        >
-        Read our white paper
-        </button>
-    </div>
+<template>
+  <div class="m-4 flex">
+    <button
+      @click="subscribeToggle"
+      class="bg-midGreen hover:bg-startGrad text-white text-center font-black px-4 py-2 mx-5 rounded-full float-left"
+    >
+      Sign up for updates
+    </button>
+    <button
+      class="bg-midGreen hover:bg-startGrad text-white text-center font-black px-4 py-2 mx-5 rounded-full float-right"
+    >
+      Read our white paper
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -36,9 +20,9 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
   methods: {
-      subscribeToggle() {
-          this.$emit("subscribeToggle")
-      }
-  }
+    subscribeToggle() {
+      this.$emit("subscribeToggle");
+    },
+  },
 });
 </script>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,27 +1,10 @@
 <template>
   <div
-    class="
-      min-h-screen
-      bg-backgroundGreen
-      py-6
-      flex flex-col
-      justify-center
-      sm:py-12
-    "
+    class="min-h-screen bg-backgroundGreen py-6 flex flex-col justify-center sm:py-12"
   >
     <div class="relative py-3 sm:max-w-s sm:mx-auto font-sans">
       <div
-        class="
-          absolute
-          inset-0
-          bg-gradient-to-r
-          from-startGrad
-          to-endGrad
-          shadow-lg
-          transform
-          -skew-y-6
-          sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl
-        "
+        class="absolute inset-0 bg-gradient-to-r from-startGrad to-endGrad shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"
       ></div>
       <div
         class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:px-20 sm:py-10"
@@ -32,16 +15,11 @@
           </div>
           <div class="divide-y divide-gray-200">
             <div
-              class="
-                py-8
-                text-base
-                leading-6
-                space-y-4
-                text-gray-700
-                sm:text-lg sm:leading-7
-              "
+              class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
             >
-              <h1 class="text-2xl font-bold text-center">Currently Under Construction</h1>
+              <h1 class="text-2xl font-bold text-center">
+                Currently Under Construction
+              </h1>
 
               <p>
                 To be the first to know about exciting new developments from
@@ -50,28 +28,22 @@
               <p>
                 If you would like to read about the reasearch and ideas behind
                 our new project please
-                <a
-                  href="#"
-                  class="
-                    underline
-                    text-midGreen
-                    hover:text-green-700
-                  "
+                <a href="#" class="underline text-midGreen hover:text-green-700"
                   >read our white paper</a
                 >
               </p>
             </div>
             <div
-              class="
-                pt-6
-                text-base
-                leading-6
-                font-bold
-                sm:text-lg sm:leading-7
-              "
+              class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7"
             >
-              <Buttons v-show="!showSubscribe" @subscribeToggle="swapComponent" />
-              <Subscribe v-show="showSubscribe" @subscribeToggle="swapComponent" />
+              <Buttons
+                v-show="!showSubscribe"
+                @subscribeToggle="swapComponent"
+              />
+              <Subscribe
+                v-show="showSubscribe"
+                @subscribeToggle="swapComponent"
+              />
             </div>
           </div>
         </div>
@@ -85,13 +57,12 @@ import { defineComponent } from "vue";
 import Buttons from "./Buttons.vue";
 import Subscribe from "./Subscribe.vue";
 
-
 export default defineComponent({
   name: "App",
   data() {
     return {
-      showSubscribe: false
-    }
+      showSubscribe: false,
+    };
   },
   components: {
     Buttons,
@@ -100,7 +71,7 @@ export default defineComponent({
   methods: {
     swapComponent() {
       this.showSubscribe = !this.showSubscribe;
-    }
-  }
+    },
+  },
 });
 </script>
