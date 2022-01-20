c02787b80e54ec5e1818f667b57f7839fdac71f9
Adam Gibbs <adam-gibbs@outlook.com>
2022-01-20 14:06:47 +0000

    fix to netlify

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/overview",
+    alias: "/",
     name: "Overview",
     component: () => import("../views/Overview.vue"),
   },
